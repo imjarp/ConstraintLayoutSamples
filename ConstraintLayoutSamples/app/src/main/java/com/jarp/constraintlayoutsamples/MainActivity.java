bb517c210b4461712d3c51b009fb4e0aad1977b7
JARP <JARP@customer-187-174-218-184.uninet-ide.com.mx>
2017-08-04 13:38:35 -0500

    Adding: Main Activity and send to the corresponding activitu Hello World Constraint Bias Contraint with any size

--- ConstraintLayoutSamples/app/src/main/java/com/jarp/constraintlayoutsamples/MainActivity.java
+++ ConstraintLayoutSamples/app/src/main/java/com/jarp/constraintlayoutsamples/MainActivity.java
@@ -1,13 +1,34 @@
 package com.jarp.constraintlayoutsamples;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                startActivity(new Intent(MainActivity.this,HelloWorldActivity.class));
+                return;
+            case R.id.button2:
+                startActivity(new Intent(MainActivity.this,HelloWorldActivity.class));
+                return;
+            default:
+                Toast.makeText(MainActivity.this,"Unknown click view",Toast.LENGTH_SHORT).show();
+        }
     }
 }
